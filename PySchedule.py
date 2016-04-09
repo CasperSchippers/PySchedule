4c10c7d305cd6278fb064dfcd157d2c2819da229
Casper Schippers <Casperschippers@hotmail.com>
2016-04-09 18:15:53 +0200

    Buttons and datetime
    Added buttons to edit the tables. Moved from date to datetime in order
    to be able to also include time in the schedule

--- PySchedule.py
+++ PySchedule.py
@@ -3,7 +3,7 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
 # import time
-from datetime import date
+from datetime import datetime
 import random
 import math
 
@@ -16,62 +16,110 @@ class Window(QWidget):
 
 	def initUI(self):
 		# define GUI here
-		self.box = QVBoxLayout()
-		self.inputbox = QHBoxLayout()
-		self.middlebuttonbox = QHBoxLayout()
-		self.outputbox = QHBoxLayout()
-		self.box.addLayout(self.inputbox)
-		self.box.addLayout(self.middlebuttonbox)
-		self.box.addLayout(self.outputbox)
+		box = QVBoxLayout()
+		inputbox = QHBoxLayout()
+		middlebuttonbox = QHBoxLayout()
+		outputbox = QHBoxLayout()
+		box.addLayout(inputbox)
+		box.addLayout(middlebuttonbox)
+		box.addLayout(outputbox)
 
 		# input box
+		## Persontabel
+		personbox = QVBoxLayout()
+		inputbox.addLayout(personbox)
+
 		self.persontable = QTableWidget()
 		self.persontable.setRowCount(0)
 		self.persontable.setColumnCount(4)
 		self.persontable.setHorizontalHeaderLabels("Naam;1;2;3".split(";"))
-		self.inputbox.addWidget(self.persontable)
+		personbox.addWidget(self.persontable)
+
+		personbuttonbox = QHBoxLayout()
+		personbox.addLayout(personbuttonbox)
+
+		addpersonrowbutton = QPushButton("Persoon toevoegen")
+		addpersonrowbutton.clicked.connect(self.addpersonrow)
+		personbuttonbox.addWidget(addpersonrowbutton)
+
+		deletepersonrowbutton = QPushButton("Persoon verwijderen")
+		deletepersonrowbutton.clicked.connect(self.removepersonrow)
+		personbuttonbox.addWidget(deletepersonrowbutton)
+
+		## Datatabel
+		databox = QVBoxLayout()
+		inputbox.addLayout(databox)
 
 		self.datatable = QTableWidget()
 		self.datatable.setRowCount(0)
 		self.datatable.setColumnCount(2)
-		self.inputbox.addWidget(self.datatable)
+		self.datatable.setHorizontalHeaderLabels(["Datum", "Tijd (optioneel)"])
+		databox.addWidget(self.datatable)
+
+		databuttonbox = QHBoxLayout()
+		databox.addLayout(databuttonbox)
+
+		adddatumrowbutton = QPushButton('Voeg rij toe')
+		adddatumrowbutton.clicked.connect(self.adddatumrow)
+		databuttonbox.addWidget(adddatumrowbutton)
+
+		deletedatumrowbutton = QPushButton('Verwijder Datum')
+		deletedatumrowbutton.clicked.connect(self.removedatumrow)
+		databuttonbox.addWidget(deletedatumrowbutton)
+
+		## Calendar
+		calbox = QVBoxLayout()
+		inputbox.addLayout(calbox)
 
 		self.cal = QCalendarWidget()
-		self.inputbox.addWidget(self.cal)
+		calbox.addWidget(self.cal)
+
+		caltimebox = QHBoxLayout()
+		inputbox.addLayout(caltimebox)
 
 		# middle button box
-		self.startschedulebutton = QPushButton('Maak rooster')
-		self.startschedulebutton.clicked.connect(self.scheduler)
-		self.middlebuttonbox.addStretch(1)
-		self.middlebuttonbox.addWidget(self.startschedulebutton)
-		self.middlebuttonbox.addStretch(1)
+		startschedulebutton = QPushButton('Maak rooster')
+		startschedulebutton.clicked.connect(self.scheduler)
+		middlebuttonbox.addStretch(1)
+		middlebuttonbox.addWidget(startschedulebutton)
+		middlebuttonbox.addStretch(1)
 
 		# output box
 		self.outputtable = QTableWidget()
 		self.outputtable.setRowCount(0)
 		self.outputtable.setColumnCount(0)
-		self.outputtable.setHorizontalHeaderLabels("Datum")
-		self.outputbox.addWidget(self.outputtable)
+		self.outputtable.setHorizontalHeaderLabels(["Datum",])
+		outputbox.addWidget(self.outputtable)
 
-		self.setLayout(self.box)
+		self.setLayout(box)
+		self.resize(1100,700)
 		self.show()
 
 	def fakedata(self):
-		self.people = ['Albert', 'Ben', 'Casper', 'Daan', 'Elroy', 'Floor', 'Geert', 'Henk', 'Ilias', 'Jonathan', 'Karel', 'Leo', 'Marc', 'Nastrobya']
-		for i in range(len(self.people)):
+		people = ['Albert', 'Ben', 'Casper', 'Daan', 'Elroy', 'Floor', 'Geert', 'Henk', 'Ilias', 'Jonathan', 'Karel', 'Leo', 'Marc', 'Nastrobya']
+		for i in range(len(people)):
 			self.persontable.insertRow(self.persontable.rowCount())
-			self.persontable.setItem(i,0, QTableWidgetItem(self.people[i]))
-		del self.people
+			self.persontable.setItem(i,0, QTableWidgetItem(people[i]))
+		# del self.people
 
-		self.dates = [date(2016, 1, 1), date(2016, 1, 2), date(2016, 1, 3), date(2016, 1, 4), date(2016, 1, 5), date(2016, 1, 6), date(2016, 1, 7)]
-		for i in range(len(self.dates)):
+		dates = [datetime(2016, 1, 1), datetime(2016, 1, 2), datetime(2016, 1, 3), datetime(2016, 1, 4), datetime(2016, 1, 5), datetime(2016, 1, 6), datetime(2016, 1, 7)]
+		for i in range(len(dates)):
 			self.datatable.insertRow(self.datatable.rowCount())
-			self.datatable.setItem(i,0, QTableWidgetItem(self.dates[i].strftime("%d/%m")))
-
+			self.datatable.setItem(i,0, QTableWidgetItem(dates[i].strftime("%d/%m/%Y")))
+		# del self.dates
 
 	def scheduler(self):
 		self.peoplenum = self.persontable.rowCount()
-		self.datesnum = len(self.dates)
+		self.datesnum = self.datatable.rowCount()
+
+		self.people = []
+		for i in range(self.peoplenum):
+			self.people.append(self.persontable.item(i,0).text())
+		
+		self.dates = []
+		for i in range(self.datesnum):
+			datestr = self.datatable.item(i,0).text()
+			self.dates.append(datetime.strptime(datestr, "%d/%m/%Y"))
 
 		self.numperday = 5
 
@@ -104,10 +152,6 @@ class Window(QWidget):
 			print('bad')
 
 	def publishschedule(self):
-		self.people = []
-		for i in range(self.peoplenum):
-			self.people.append(self.persontable.item(i,0).text())
-		
 		self.outputtable.setColumnCount(self.numperday+1)
 		self.outputtable.setRowCount(self.datesnum)
 
@@ -117,7 +161,7 @@ class Window(QWidget):
 		self.outputtable.setHorizontalHeaderLabels(HeaderString.split(';'))
 
 		for i in range(len(self.schedule)):
-			self.outputtable.setItem(i,0, QTableWidgetItem(self.dates[i].strftime("%d / %m / %Y")))
+			self.outputtable.setItem(i,0, QTableWidgetItem(self.dates[i].strftime("%d / %m / %Y  %H:%M")))
 
 			for j in range(len(self.schedule[i])):
 				self.outputtable.setItem(i,j+1, QTableWidgetItem(self.people[self.schedule[i][j]]))
@@ -125,6 +169,20 @@ class Window(QWidget):
 
 	def checkschedule(self):
 		return True
+
+	def addpersonrow(self):
+		self.persontable.insertRow(self.persontable.rowCount())
+
+	def removepersonrow(self):
+		selected = self.persontable.currentRow()
+		self.persontable.removeRow(selected)
+
+	def adddatumrow(self):
+		self.datatable.insertRow(self.datatable.rowCount())
+
+	def removedatumrow(self):
+		selected = self.datatable.currentRow()
+		self.datatable.removeRow(selected)
 
 	# def correctschedule(self):
 
